ed426221d35d5989af75ee7bb44b91871ad383b3
starsoi <starsoi@gmail.com>
2021-05-01 20:00:21 +0200

    add unit tests

---
+++ test/test_trace_events.py
@@ -0,0 +1,48 @@
+import unittest
+from trace.tef import *
+
+
+class TestTraceEvents(unittest.TestCase):
+    def test_duration_begin_events(self):
+        ts = 123
+        event = TraceEventDurationBegin('duration-event', ts)
+        self.assertEqual(event.ph, 'B')
+        self.assertEqual(event.ts, ts * 1e6)
+
+        event.pid = 1
+        event.tid = 2
+
+        self.assertDictEqual(event.tef,
+                             {'name': 'duration-event', 'ph': 'B', 'pid': 1, 'tid': 2, 'ts': ts * 1e6, 'args': {}}
+                             )
+
+    def test_duration_end_events(self):
+        ts = 124
+        event = TraceEventDurationEnd('duration-event', ts)
+        self.assertEqual(event.ph, 'E')
+        self.assertEqual(event.ts, ts * 1e6)
+
+    def test_instant_event(self):
+        ts = 123
+        event = TraceEventInstant('instant-event', ts)
+        self.assertEqual(event.ph, 'i')
+        self.assertTrue(hasattr(event, 's'))
+        self.assertEqual(event.ts, ts * 1e6)
+
+    def test_event_sequence(self):
+        tracer = Tracer()
+        event1_begin = TraceEventDurationBegin('event1', 123)
+        event1_end = TraceEventDurationEnd('event1', 124)
+        event2 = TraceEventInstant('event2', 123.5)
+
+        tracer.add_event(event1_begin)
+        tracer.add_event(event1_end)
+        tracer.add_event(event2)
+
+        self.assertIs(tracer.events[0], event1_begin)
+        self.assertIs(tracer.events[1], event1_end)
+        self.assertIs(tracer.events[2], event2)
+
+
+if __name__ == '__main__':
+    unittest.main()
