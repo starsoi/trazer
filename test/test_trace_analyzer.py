c579dcb655efc23b014d8c6f4d4716ab19fb388e
starsoi <starsoi@gmail.com>
2021-05-02 20:33:27 +0200

    correct expected and actual value in unit tests

--- test/test_trace_analyzer.py
+++ test/test_trace_analyzer.py
@@ -19,35 +19,35 @@ class TestTraceEvents(unittest.TestCase):
     def test_event_name_codes_1(self):
         tracer = self.setup_tracer(n_events=1)
         trace_analyzer = TraceAnalyzer(tracer)
-        self.assertDictEqual(trace_analyzer.event_name_codes, {'event000': 'A'})
+        self.assertDictEqual({'event000': 'A'}, trace_analyzer.event_name_codes)
 
     def test_event_name_codes_10(self):
         tracer = self.setup_tracer(n_events=10)
         trace_analyzer = TraceAnalyzer(tracer)
         self.assertDictEqual(
-            trace_analyzer.event_name_codes,
             {
                 'event000': 'A', 'event001': 'B', 'event002': 'C', 'event003': 'D', 'event004': 'E',
                 'event005': 'F', 'event006': 'G', 'event007': 'H', 'event008': 'I', 'event009': 'J',
-            }
+            },
+            trace_analyzer.event_name_codes
         )
 
     def test_event_name_codes_104(self):
         tracer = self.setup_tracer(n_events=104)
         trace_analyzer = TraceAnalyzer(tracer)
         event_name_codes = trace_analyzer.event_name_codes
-        self.assertEqual(event_name_codes['event000'], 'AA')
-        self.assertEqual(event_name_codes['event025'], 'AZ')
-        self.assertEqual(event_name_codes['event026'], 'Aa')
-        self.assertEqual(event_name_codes['event051'], 'Az')
-        self.assertEqual(event_name_codes['event052'], 'BA')
-        self.assertEqual(event_name_codes['event103'], 'Bz')
+        self.assertEqual('AA', event_name_codes['event000'])
+        self.assertEqual('AZ', event_name_codes['event025'])
+        self.assertEqual('Aa', event_name_codes['event026'])
+        self.assertEqual('Az', event_name_codes['event051'])
+        self.assertEqual('BA', event_name_codes['event052'])
+        self.assertEqual('Bz', event_name_codes['event103'])
 
     def test_event_string(self):
         tracer = self.setup_tracer(n_events=3)
         tracer.add_event(TraceEventInstant('event999', 1))
         trace_analyzer = TraceAnalyzer(tracer)
-        self.assertEqual(trace_analyzer.events_string, 'A+B+C+C-B-A-D!')
+        self.assertEqual('A+B+C+C-B-A-D!', trace_analyzer.events_string)
 
     def test_event_name_validation(self):
         tracer = Tracer()
