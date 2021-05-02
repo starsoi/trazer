17bfdc34cd4b93f03a6cd76a383b252541c7b793
starsoi <starsoi@gmail.com>
2021-05-02 20:30:22 +0200

    correct expected and actual value in unit tests

--- test/test_trace_events.py
+++ test/test_trace_events.py
@@ -18,30 +18,30 @@ class TestTraceEvents(unittest.TestCase):
         event.pid = 1
         event.tid = 2
 
-        self.assertDictEqual(event.tef,
-                             {'name': 'duration-event', 'ph': 'B', 'pid': 1, 'tid': 2, 'ts': ts * 1e6, 'args': {}}
+        self.assertDictEqual({'name': 'duration-event', 'ph': 'B', 'pid': 1, 'tid': 2, 'ts': ts * 1e6, 'args': {}},
+                             event.tef,
                              )
 
     def test_duration_end_events(self):
         ts = 124
         event = TraceEventDurationEnd('duration-event', ts)
-        self.assertEqual(event.tef['ph'], 'E')
-        self.assertEqual(event.tef['ts'], ts * 1e6)
+        self.assertEqual('E', event.tef['ph'])
+        self.assertEqual(ts * 1e6, event.tef['ts'])
 
     def test_instant_event(self):
         ts = 123
         event = TraceEventInstant('instant-event', ts)
-        self.assertEqual(event.tef['ph'], 'i')
+        self.assertEqual('i', event.tef['ph'])
         self.assertIn('s', event.tef)
-        self.assertEqual(event.tef['ts'], ts * 1e6)
+        self.assertEqual(ts * 1e6, event.tef['ts'])
 
     def test_counter_event(self):
         ts = 123
         event_name = 'counter-event'
         counter_value = 999
         event = TraceEventCounter(event_name, ts, counter_value)
-        self.assertEqual(event.tef['ph'], 'C')
-        self.assertDictEqual(event.tef, {**event.tef, 'args': {event_name: counter_value}})
+        self.assertEqual('C', event.tef['ph'])
+        self.assertDictEqual({**event.tef, 'args': {event_name: counter_value}}, event.tef)
 
     def test_event_sequence(self):
         tracer = Tracer()
@@ -59,7 +59,7 @@ class TestTraceEvents(unittest.TestCase):
 
     def test_custom_attributes(self):
         event = TraceEventDurationEnd('event', 0, custom_attr1='foo', custom_attr2=100)
-        self.assertDictEqual(event.tef['args'], {'custom_attr1': 'foo', 'custom_attr2': 100})
+        self.assertDictEqual({'custom_attr1': 'foo', 'custom_attr2': 100}, event.tef['args'])
 
     def test_json_export(self):
         import json
@@ -73,9 +73,9 @@ class TestTraceEvents(unittest.TestCase):
         tracer.add_event(event2)
 
         readback = json.loads(tracer.json)
-        self.assertDictEqual(readback['traceEvents'][0], event1_begin.tef)
-        self.assertDictEqual(readback['traceEvents'][1], event1_end.tef)
-        self.assertDictEqual(readback['traceEvents'][2], event2.tef)
+        self.assertDictEqual(event1_begin.tef, readback['traceEvents'][0])
+        self.assertDictEqual(event1_end.tef, readback['traceEvents'][1])
+        self.assertDictEqual(event2.tef, readback['traceEvents'][2])
 
 
 if __name__ == '__main__':
