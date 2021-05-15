b015244f5824d4cdb70ba403a9326c77d4a33d64
starsoi <starsoi@gmail.com>
2021-05-15 18:01:26 +0200

    add docstrings for trace.py

--- trazer/trace.py
+++ trazer/trace.py
@@ -5,24 +5,48 @@ import json
 
 
 class Trace(object):
+    """A ``Trace`` contains all collected trace events and can be exported into different trace file formats for
+    further processing in other tools, e.g. for visualization.
+    """
+
     def __init__(self):
+        """Initialize a ``Trace`` instance with the following properties:
+
+        * ``events``: A list of trace events, which has the insertion order of the contained trace events.
+        """
         self.events: List[TraceEvent] = []
 
     def add_event(self, event: TraceEvent):
+        """Add a trace event into the trace.
+
+        :param event: The event instance to be appended.
+        :return: None
+        """
         self.events.append(event)
 
     @property
     def tef(self):
+        """Get a dict containing the properties required for the Trace Event Format.
+
+        :return: A dict with properties from Trace Event Format
+        """
         import trazer.export as export
 
         return export.to_tef(self)
 
     @property
     def tef_json(self):
+        """Get the JSON string of ``Trace.tef``.
+        The string can be written into a JSON file for the visualization in Chrome ```chrome://tracing```.
+
+        :return: The JSON string.
+        """
         return json.dumps(self.tef, indent=4)
 
 
 class TraceEvent(ABC):
+    """An abstract class containing the basic properties for a trace event."""
+
     def __init__(self, name: str, ts: float, pid: int = 0, tid: int = 0, **kwargs):
         self.name = name
         self._ts = ts  # original input timestamp in seconds
@@ -33,11 +57,20 @@ class TraceEvent(ABC):
 
     @property
     def tef(self):
+        """Get a dict containing the properties required for the Trace Event Format.
+
+        :return: A dict with properties from Trace Event Format
+        """
         import trazer.export as export
 
         return export.to_tef(self)
 
     def __str__(self):
+        """Get the string representation of this event.
+        It has a format: ``[<timestamp>]: <name of the event> (shortname of the event)``
+
+        :return: The string representation.
+        """
         return f'[{self.ts} us]: {self.name} ({self._shortname})'
 
     def __repr__(self):
