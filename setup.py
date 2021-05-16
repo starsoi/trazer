ad8ccdbfc09ff98c2f0b29e9c0f4d500560719a7
starsoi <starsoi@gmail.com>
2021-05-16 20:02:59 +0200

    add initial pypi packaging setup

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="trazer",
+    version="0.1.1a",
+    author="Hongyan Zhang",
+    author_email="starsoi@gmail.com",
+    description="A generic trace analysis framework.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/starsoi/trazer",
+    project_urls={},
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Embedded Systems",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Intended Audience :: Developers",
+    ],
+    package_dir={"": "."},
+    packages=["trazer"],
+    python_requires=">=3.8",
+)
