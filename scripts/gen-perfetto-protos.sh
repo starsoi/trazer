22f85dd836daefb624283f06578c9d95af20e897
starsoi <starsoi@gmail.com>
2023-04-22 13:04:21 +0200

    chore: update script to remove non-proto files

--- scripts/gen-perfetto-protos.sh
+++ scripts/gen-perfetto-protos.sh
@@ -7,7 +7,15 @@ TRAZER_PROTO_PATH="$SCRIPT_DIR/../protos"
 
 tmpdir=$(mktemp -d)
 
+# Clone the Perfetto repository from GitHub to a temporary folder
 git clone $PERFETTO_REPO_URL $tmpdir
+
+# Copy all trace-related protos to the project folder
 cp -r $tmpdir/$PERFETTO_TRACE_PROTO_PATH $TRAZER_PROTO_PATH
+
+# Remove all non-proto files
+find $TRAZER_PROTO_PATH -not -iname "*.proto" -type f -delete
+
+# Cleanup the temporary folder
 rm -rf $tmpdir
 
